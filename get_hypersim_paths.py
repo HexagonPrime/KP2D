2af28fc68b5d610377d7c6105f0825412ccdf8ea
HexagonPrime <hexagonprime@gmail.com>
2021-04-15 15:17:56 -0700

    add hyperim structure converting script

--- get_hypersim_paths.py
+++ get_hypersim_paths.py
@@ -10,7 +10,7 @@ for scene in os.listdir("/cluster/scratch/shecai/hypersim"):
     for directory in os.listdir(scene_images_dir):
         if directory.endswith("_final_hdf5"):
             cam_trajectory_dir = os.path.join(scene_images_dir, directory)
-            for image in os.listdir(cam_trajectory_dir)
+            for image in os.listdir(cam_trajectory_dir):
                 image_path = os.path.join(cam_trajectory_dir, image)
                 images_dict_list.append([scene, cam_trajectory_dir, image_path])
 print (images_dict_list)
