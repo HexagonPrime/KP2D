023c29351eace70bc4f9d662b810345560a73436
HexagonPrime <hexagonprime@gmail.com>
2021-04-15 14:54:29 -0700

    add hyperim structure converting script

--- get_hypersim_paths.py
+++ get_hypersim_paths.py
@@ -3,5 +3,6 @@ import os
 scene_images_dir_list = []
 for scene in os.listdir("/cluster/scratch/shecai/hypersim"):
     scene_dir = os.path.join("/cluster/scratch/shecai/hypersim", scene)
+    print (scene_dir)
     scene_images_dir_list.append(os.path.join(scene_dir, "/images"))
 print(scene_images_dir_list)
