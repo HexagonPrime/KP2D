324d71a14bddd3a9d485dbfb1fca88eca141b73a
HexagonPrime <hexagonprime@gmail.com>
2021-04-16 08:07:44 -0700

    update

--- test_hypersim.py
+++ test_hypersim.py
@@ -9,11 +9,13 @@ df = pd.read_csv('/cluster/scratch/shecai/hypersim/hypersim.csv', names=col_list
 files = df["img_path"].tolist()
 for filename in files:
     h5 = h5py.File(filename, 'r')
-    print(filename)
+    # print(filename)
     img_array = np.array(h5['dataset'][:], dtype='f')
     img_array = img_array * 255
     img_array = img_array.astype(np.uint8)
     # print(img_array.shape)
     img = Image.fromarray(img_array)
     img = img.resize((int(img.size[0]/3), int(img.size[1]/3)))
-    print (img.size)
+    # print (img.size)
+    if not (img.size[0] == 341 and img.size[1] == 256):
+        print("false")
