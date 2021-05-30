8f7b94b403be71ba610264dcf2b0610c47623e99
HexagonPrime <8053335677@qq.com>
2021-05-30 19:26:18 +0100

    add learning from scratch model

---
+++ mode_con_scratch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+#SBATCH --output=con_scratch.out
+#SBATCH --mem=40G
+#SBATCH --gres=gpu:1
+#SBATCH --constrain='titan_xp|geforce_gtx_titan_x|geforce_rtx_2080_ti|titan_x|titan_xp|'
+
+source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
+conda activate kp2d
+python train_keypoint_net.py --file kp2d/configs/v4.yaml --training_mode consecutive
